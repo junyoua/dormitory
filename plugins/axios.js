e824f931b5e1a76e158eb8e3d64d38784bf8c8bd
You <1687144268@qq.com>
2022-05-08 18:17:04 +0800

    three commit

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,7 +4,7 @@ import qs from 'qs'
 axios.defaults.withCredentials = true
 
 // axios请求添加token
-export default function ({ $axios, redirect, store }) {
+export default function ({ $axios, redirect, router, store }) {
   $axios.onRequest((config) => {
     const token = store.state.token
     if (token) {
@@ -16,4 +16,20 @@ export default function ({ $axios, redirect, store }) {
     })
     return config
   })
+  // 解决报401错误
+  $axios.interceptors.response.use(function (response) {
+    return response
+  }, function (error) {
+    if (error.response.status === 401) {
+      store.commit('uptoken', null)
+      localStorage.clear()
+      const data = '登录状态失效,请重新登录!'
+      redirectLogin()
+      return Promise.reject(data)
+    }
+  })
+}
+// 401报错重定向登录页面
+function redirectLogin () {
+  window.location.replace('/login')
 }
